3410e26efc6a3bfc73b9dceb20dc5dd65005e36d
Nick Rowe <nixterrimus@dcxn.com>
2013-03-21 10:52:27 -0700

    A base, booting ember application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,17 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
+//= require moment
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
 //= require_tree .
+
+TimeTravel = Ember.Application.create();
+
+TimeTravel.Store = DS.Store.extend({
+  revision: 12,
+  adapter: DS.RESTAdapter.create()
+});
