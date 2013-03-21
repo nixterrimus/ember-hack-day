d17069b88155a37999029e4a5a649b827af3997e
Nick Rowe <nixterrimus@dcxn.com>
2013-03-21 10:35:43 -0700

    First commit; Base Rails, ember installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberDay::Application.config.secret_token = '007b25c08a680c9708d7264c949a6123f23f4c7311a127361ea5f69218c31e58f75a29bba058736e023ecca99b1d92cae79bbd645338cd1b8208c9d1084e3ed2'
