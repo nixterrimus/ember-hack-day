e3093334dacf0f56989cf2522dd93cf83fed8e10
Nick Rowe <nixterrimus@dcxn.com>
2013-03-21 14:38:17 -0700

    Update trip serializer to only embed ids

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -2,4 +2,5 @@ class TripSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :start_date, :end_date, :slug, :tagLine, :price, :location, :activity
   has_many :hotels
   has_many :extras
+  embed :ids
 end
